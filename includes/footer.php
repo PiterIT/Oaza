b06f69285ffa29c9d187283b063090a2546b1e2d
Piter <piotr.redzimski@onet.eu>
2018-03-22 20:06:08 +0100

    Zebranie
    Nowe ogłoszenie o walnym zebraniu

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <div style="clear: both;"></div>
 <div id="footer">
-	&copy; 2017. Wszelkie prawa zastrzeżone - inż. Piotr Redzimski / +48 604 581 567 /<a href="admin.php" style="color: blue"> Kontakt z administratorem strony</a>
+	&copy; 2018. Wszelkie prawa zastrzeżone - inż. Piotr Redzimski / +48 604 581 567 /<a href="admin.php" style="color: blue"> Kontakt z administratorem strony</a>
 </div>
 
