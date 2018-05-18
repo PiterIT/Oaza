64b1f2769b2511842387973ec89cdde75c7c9243
Piter <piotr.redzimski@onet.eu>
2018-05-19 00:43:18 +0200

    2018-05-19
    Dodano Slider

--- galeria.php
+++ galeria.php
@@ -5,9 +5,49 @@
 <?php include('includes/body.php');?>
 
 		<div id="content_right">
-			<h1>Galeria</h1><br />
-			Nasze ogrody<br />
-			Na tej stronie będzie można umieszczać zdjęcia naszych ogrodów
+			<!-- <h1>Nasze ogrody</h1> -->
+			<div id="wrapper">
+				<div id="slider-wrap">
+					<ul id="slider">
+						<li data-color="#1abc9c">
+							<div>
+								<h3>#227</h3>
+								<span>Altana stara tył</span>
+							</div>
+							<img src="images/6_1.jpg" alt="Altana stara tył"  width="600" height="400"/>
+						</li>	
+						<li data-color="#3498db">
+							<div>
+								<h3>#227</h3>
+								<span>Altana nowa tył</span>
+							</div>
+							<img src="images/6_2.jpg" alt="Altana nowa tył"  width="600" height="400"/>
+						</li>
+						<li data-color="#9b59b6">
+							<div>
+								<h3>#227</h3>
+								<span>Altana stara przód</span>
+							</div>
+							<img src="images/6_3.jpg" alt="Altana stara przód"  width="600" height="400"/>
+						</li>
+						<li data-color="#34495e">
+							<div>
+								<h3>#227</h3>
+								<span>Altana nowa przód</span>
+							</div>
+							<img src="images/6_4.jpg" alt="Altana nowa przód"  width="600" height="400"/>
+						</li>
+					</ul>
+					<!--controls-->
+					<div class="btns" id="next"><i class="fa fa-arrow-right"></i></div>
+					<div class="btns" id="previous"><i class="fa fa-arrow-left"></i></div>
+					<div id="counter"></div>
+					<div id="pagination-wrap">
+						<ul>
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 
--- includes/head.php
+++ includes/head.php
@@ -8,8 +8,14 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
 	<link rel="stylesheet" href="css/style.css"  type="text/css" />
+
+	<link href="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" rel="stylesheet"><!-- Simple Image Slider -->
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"><!-- Simple Image Slider -->
+	<link href='https://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'><!-- Simple Image Slider -->
+	<link href='https://fonts.googleapis.com/css?family=Neucha' rel='stylesheet' type='text/css'><!-- Simple Image Slider -->
 	
 	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script src="js/timer.js"></script>
+	<script src="js/timer.js"></script>
+	<script type="module" src="js/slider.js"></script>
 </head>
